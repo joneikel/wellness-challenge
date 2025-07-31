06e15d9b18d360ba7b663101579307a753437c55
joneikel <joneikel2010@gmail.com>
2025-07-31 14:48:29 -0400

    [ADD] Configuracion de swagger y reglas de eslint

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Api gateway')
+    .setDescription('Documentación de la API gateway')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('waggwer/api-docs', app, document);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
